fae17880946c91648d9fca21550d121cd7fb4fbe
Diether012524 <labisdiether345@gmail.com>
2024-11-18 20:32:42 +0800

    modified

--- scripts.js
+++ scripts.js
@@ -1,27 +1,21 @@
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-
 let currentIndex = 0;
 const images = document.querySelectorAll('.slider-image'); 
 
-document.querySelector('.position-absolute.start-0').addEventListener('click', () => {
+
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
+
+
+prevButton.addEventListener('click', () => {
     currentIndex = (currentIndex > 0) ? currentIndex - 1 : images.length - 1;
     updateSlider();
 });
 
-document.querySelector('.position-absolute.end-0').addEventListener('click', () => {
+nextButton.addEventListener('click', () => {
     currentIndex = (currentIndex < images.length - 1) ? currentIndex + 1 : 0;
     updateSlider();
 });
+
 
 function updateSlider() {
     images.forEach((img, index) => {
